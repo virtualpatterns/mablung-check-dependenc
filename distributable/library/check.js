04262d2244cdc13d260cbedcd092b50c08b30cdf
virtualpatterns <code.virtualpatterns@gmail.com>
2020-08-07 21:34:59 -0400

    post-test

--- distributable/library/check.js
+++ distributable/library/check.js
@@ -69,7 +69,6 @@ export function Check(userPath = Process.cwd(), userOption = {}) {
 
           if (Is.emptyObject(unused.invalidDirs)) {
             let fileError = Object.entries(unused.invalidFiles);
-            console.log(fileError[0][0]);
             reject(fileError[0][1]);
           } else {
             let folderError = Object.entries(unused.invalidDirs);
