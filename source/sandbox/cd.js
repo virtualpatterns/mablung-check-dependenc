149cf7dbbb06c1e1530d14f5e6b34097ad3a0383
virtualpatterns <code.virtualpatterns@gmail.com>
2020-08-07 21:35:55 -0400

    stuff

--- source/sandbox/cd.js
+++ source/sandbox/cd.js
@@ -1,11 +1,11 @@
 // import '@virtualpatterns/mablung-source-map-support/install'
 import Check from 'depcheck'
-import FileSystem from 'fs-extra'
-import Is from '@pwn/is'
-import JSON5 from 'json5'
-import Match from 'minimatch'
-import Path from 'path'
-import Query from 'jsonpath'
+// import FileSystem from 'fs-extra'
+// import Is from '@pwn/is'
+// import JSON5 from 'json5'
+// import Match from 'minimatch'
+// import Path from 'path'
+// import Query from 'jsonpath'
 
 // const FilePath = __filePath
 // const FolderPath = Path.dirname(FilePath)
@@ -23,59 +23,60 @@ const Process = process
 
 // }
 
-async function parseStaticBabelConfiguration(filePath) {
-  // console.log(`parseStaticBabelConfiguration('${Path.relative('', filePath)}') { ... }`)
+// async function parseStaticBabelConfiguration(filePath) {
+//   // console.log(`parseStaticBabelConfiguration('${Path.relative('', filePath)}') { ... }`)
 
-  let dependency = []
+//   let dependency = []
 
-  let fileName = Path.basename(filePath)
-  let pattern = [ 'package.json', 'babel.config.json', '.babelrc.json', 'poop*.json' ]
+//   let fileName = Path.basename(filePath)
+//   let pattern = [ 'package.json', 'babel.config.json', '.babelrc.json', 'poop*.json' ]
 
-  if (pattern.reduce((isMatch, pattern) => isMatch ? isMatch : Match(fileName, pattern, { 'dot': true }), false)) {
+//   if (pattern.reduce((isMatch, pattern) => isMatch ? isMatch : Match(fileName, pattern, { 'dot': true }), false)) {
 
-    let configuration = null
-    configuration = JSON5.parse(await FileSystem.readFile(filePath, { 'encoding': 'utf-8' }))
-    configuration = fileName === 'package.json' ? (configuration.babel || {}) : configuration
+//     let configuration = null
+//     configuration = JSON5.parse(await FileSystem.readFile(filePath, { 'encoding': 'utf-8' }))
+//     configuration = fileName === 'package.json' ? (configuration.babel || {}) : configuration
 
-    let plugin = null
-    plugin = Query.query(configuration, '$..plugins[*]')
-    plugin = plugin.filter((plugin) => Is.array(plugin) || plugin !== 'importMeta')
-    plugin = plugin.map((plugin) => Is.array(plugin) ? plugin[0] : plugin)
+//     let plugin = null
+//     plugin = Query.query(configuration, '$..plugins[*]')
+//     plugin = plugin.filter((plugin) => Is.array(plugin) || plugin !== 'importMeta')
+//     plugin = plugin.map((plugin) => Is.array(plugin) ? plugin[0] : plugin)
 
-    let preset = null
-    preset = Query.query(configuration, '$..presets[*]')
-    preset = preset.map((preset) => Is.array(preset) ? preset[0] : preset)
+//     let preset = null
+//     preset = Query.query(configuration, '$..presets[*]')
+//     preset = preset.map((preset) => Is.array(preset) ? preset[0] : preset)
 
-    dependency = [...plugin, ...preset]
+//     dependency = [...plugin, ...preset]
 
-  }
+//   }
 
-  return dependency
+//   return dependency
 
-}
+// }
 
 async function main() {
 
   try {
 
-    let option = {
-      'parsers': {
-        '*.js': Check.parser.es6,
-        '*.cjs': Check.parser.es6
-      },
-      'specials': [
-        Check.special.bin,
-        Check.special.eslint,
-        parseStaticBabelConfiguration
-      ]
-    }
+    let option = {}
+    //   'parsers': {
+    //     '*.js': Check.parser.es6
+    //     ,
+    //     '*.cjs': Check.parser.es6
+    //   },
+    //   'specials': [
+    //     Check.special.bin,
+    //     Check.special.eslint
+    //     // ,
+    //     // parseStaticBabelConfiguration
+    //   ]
+    // }
 
     Check(Process.cwd(), option, (unused) => {
       console.dir(unused.dependencies) // an array containing the unused dependencies
       console.dir(unused.devDependencies) // an array containing the unused devDependencies
       console.dir(unused.missing) // a lookup containing the dependencies missing in `package.json` and where they are used
       // console.dir(unused.using) // a lookup indicating each dependency is used by which files
-      debugger
       console.dir(unused.invalidFiles) // files that cannot access or parse
       console.dir(unused.invalidDirs) // directories that cannot access
     })
