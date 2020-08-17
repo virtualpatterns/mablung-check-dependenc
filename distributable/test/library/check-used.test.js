f4d27131161740fec8620551b522806042f16a5e
virtualpatterns <code.virtualpatterns@gmail.com>
2020-08-16 23:21:35 -0400

    post-test

--- distributable/test/library/check-used.test.js
+++ distributable/test/library/check-used.test.js
@@ -16,7 +16,7 @@ Test('Check(\'used/ava\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-ava': [`${ResourcePath}/used/ava/package.json`] } });
+      '@virtualpatterns/mablung-ava': [Path.join(ResourcePath, 'used', 'ava', 'package.json')] } });
 
 
 });
@@ -26,7 +26,7 @@ Test('Check(\'used/ava.config.json\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-ava': [`${ResourcePath}/used/ava.config.json/ava.config.json`] } });
+      '@virtualpatterns/mablung-ava': [Path.join(ResourcePath, 'used', 'ava.config.json', 'ava.config.json')] } });
 
 
 });
@@ -36,7 +36,7 @@ Test('Check(\'used/babel-environment-plugin\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-babel-environment-plugin': [`${ResourcePath}/used/babel-environment-plugin/package.json`] } });
+      '@virtualpatterns/mablung-babel-environment-plugin': [Path.join(ResourcePath, 'used', 'babel-environment-plugin', 'package.json')] } });
 
 
 });
@@ -46,7 +46,7 @@ Test('Check(\'used/babel-environment-preset\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-babel-environment-preset': [`${ResourcePath}/used/babel-environment-preset/package.json`] } });
+      '@virtualpatterns/mablung-babel-environment-preset': [Path.join(ResourcePath, 'used', 'babel-environment-preset', 'package.json')] } });
 
 
 });
@@ -57,7 +57,7 @@ Test('Check(\'used/babel-plugin\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-babel-plugin': [`${ResourcePath}/used/babel-plugin/package.json`] } });
+      '@virtualpatterns/mablung-babel-plugin': [Path.join(ResourcePath, 'used', 'babel-plugin', 'package.json')] } });
 
 
 });
@@ -67,7 +67,7 @@ Test('Check(\'used/babel-preset\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-babel-preset': [`${ResourcePath}/used/babel-preset/package.json`] } });
+      '@virtualpatterns/mablung-babel-preset': [Path.join(ResourcePath, 'used', 'babel-preset', 'package.json')] } });
 
 
 });
@@ -77,7 +77,7 @@ Test('Check(\'used/babel.config.json-environment-plugin\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-babel-environment-plugin': [`${ResourcePath}/used/babel.config.json-environment-plugin/babel.config.json`] } });
+      '@virtualpatterns/mablung-babel-environment-plugin': [Path.join(ResourcePath, 'used', 'babel.config.json-environment-plugin', 'babel.config.json')] } });
 
 
 });
@@ -87,7 +87,7 @@ Test('Check(\'used/babel.config.json-environment-preset\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-babel-environment-preset': [`${ResourcePath}/used/babel.config.json-environment-preset/babel.config.json`] } });
+      '@virtualpatterns/mablung-babel-environment-preset': [Path.join(ResourcePath, 'used', 'babel.config.json-environment-preset', 'babel.config.json')] } });
 
 
 });
@@ -97,7 +97,7 @@ Test('Check(\'used/babel.config.json-plugin\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-babel-plugin': [`${ResourcePath}/used/babel.config.json-plugin/babel.config.json`] } });
+      '@virtualpatterns/mablung-babel-plugin': [Path.join(ResourcePath, 'used', 'babel.config.json-plugin', 'babel.config.json')] } });
 
 
 });
@@ -107,7 +107,7 @@ Test('Check(\'used/babel.config.json-preset\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-babel-preset': [`${ResourcePath}/used/babel.config.json-preset/babel.config.json`] } });
+      '@virtualpatterns/mablung-babel-preset': [Path.join(ResourcePath, 'used', 'babel.config.json-preset', 'babel.config.json')] } });
 
 
 });
@@ -117,7 +117,7 @@ Test('Check(\'used/babelrc.json-environment-plugin\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-babel-environment-plugin': [`${ResourcePath}/used/babelrc.json-environment-plugin/.babelrc.json`] } });
+      '@virtualpatterns/mablung-babel-environment-plugin': [Path.join(ResourcePath, 'used', 'babelrc.json-environment-plugin', '.babelrc.json')] } });
 
 
 });
@@ -127,7 +127,7 @@ Test('Check(\'used/babelrc.json-environment-preset\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-babel-environment-preset': [`${ResourcePath}/used/babelrc.json-environment-preset/.babelrc.json`] } });
+      '@virtualpatterns/mablung-babel-environment-preset': [Path.join(ResourcePath, 'used', 'babelrc.json-environment-preset', '.babelrc.json')] } });
 
 
 });
@@ -137,7 +137,7 @@ Test('Check(\'used/babelrc.json-plugin\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-babel-plugin': [`${ResourcePath}/used/babelrc.json-plugin/.babelrc.json`] } });
+      '@virtualpatterns/mablung-babel-plugin': [Path.join(ResourcePath, 'used', 'babelrc.json-plugin', '.babelrc.json')] } });
 
 
 });
@@ -147,7 +147,7 @@ Test('Check(\'used/babelrc.json-preset\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-babel-preset': [`${ResourcePath}/used/babelrc.json-preset/.babelrc.json`] } });
+      '@virtualpatterns/mablung-babel-preset': [Path.join(ResourcePath, 'used', 'babelrc.json-preset', '.babelrc.json')] } });
 
 
 });
@@ -157,7 +157,7 @@ Test('Check(\'used/dependency\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      '@virtualpatterns/mablung-dependency': [`${ResourcePath}/used/dependency/dependency.js`] } });
+      '@virtualpatterns/mablung-dependency': [Path.join(ResourcePath, 'used', 'dependency/dependency.js')] } });
 
 
 });
@@ -169,8 +169,8 @@ Test('Check(\'used/parcel\')', async test => {
     'parcel-bundler'],
 
     'used': {
-      '@studysync/parcel-plugin-bundle-visualiser': [`${ResourcePath}/used/parcel/package.json`],
-      'parcel-plugin-asset-copier': [`${ResourcePath}/used/parcel/package.json`] } });
+      '@studysync/parcel-plugin-bundle-visualiser': [Path.join(ResourcePath, 'used', 'parcel', 'package.json')],
+      'parcel-plugin-asset-copier': [Path.join(ResourcePath, 'used', 'parcel', 'package.json')] } });
 
 
 });
@@ -180,7 +180,7 @@ Test('Check(\'used/pug\')', async test => {
     'missing': {},
     'unused': [],
     'used': {
-      'jstransformer-markdown-it': [`${ResourcePath}/used/pug/template.pug`] } });
+      'jstransformer-markdown-it': [Path.join(ResourcePath, 'used', 'pug/template.pug')] } });
 
 
 });
