be3837d253c8e9838581d1d337b9f4ecc66a8ec4
virtualpatterns <code.virtualpatterns@gmail.com>
2020-11-11 20:08:35 -0500

    post-test

--- distributable/command/check-dependency.js
+++ distributable/command/check-dependency.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import '@virtualpatterns/mablung-source-map-support/install';
+import { createRequire as CreateRequire } from 'module';
 import Command from 'commander';
 import FileSystem from 'fs-extra';
 import Is from '@pwn/is';
@@ -9,8 +10,10 @@ import Path from 'path';
 
 import { Check } from '../library/check.js';
 
-const Package = JSON5.parse(FileSystem.readFileSync('package.json'), { 'encoding': 'utf-8' });
 const Process = process;
+const Require = CreateRequire(import.meta.url);
+
+const Package = JSON5.parse(FileSystem.readFileSync(Require.resolve('../../package.json')), { 'encoding': 'utf-8' });
 
 Command.
 version(Package.version).
