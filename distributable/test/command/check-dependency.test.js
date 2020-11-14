5da7283e582fdc0985f164ea5ca5bbeb66678ca5
virtualpatterns <code.virtualpatterns@gmail.com>
2020-11-13 23:42:04 -0500

    post-test

--- distributable/test/command/check-dependency.test.js
+++ distributable/test/command/check-dependency.test.js
@@ -12,57 +12,57 @@ const FolderPath = Path.dirname(FilePath);
 // dot files (e.g. .babelrc.json)
 const ResourcePath = Path.normalize(`${FolderPath}/resource`);
 
-Test.serial('check-dependency --help', async test => {
+Test('check-dependency --help', async test => {
   let process = new CheckDependencyProcess({ '--help': true });
   test.is(await process.whenExit(), 0);
 });
 
-Test.serial('check-dependency', async test => {
+Test('check-dependency', async test => {
   let process = new CheckDependencyProcess();
   test.is(await process.whenExit(), 0);
 });
 
-Test.serial('check-dependency --project-path no-issue', async test => {
+Test('check-dependency --project-path no-issue', async test => {
   let process = new CheckDependencyProcess({ '--project-path': `${ResourcePath}/no-issue` });
   test.is(await process.whenExit(), 0);
 });
 
-Test.serial('check-dependency --project-path unused', async test => {
+Test('check-dependency --project-path unused', async test => {
   let process = new CheckDependencyProcess({ '--project-path': `${ResourcePath}/unused` });
   test.is(await process.whenExit(), 1);
 });
 
-Test.serial('check-dependency --project-path unused --configuration-path unused/configuration.json', async test => {
+Test('check-dependency --project-path unused --configuration-path unused/configuration.json', async test => {
   let process = new CheckDependencyProcess({ '--project-path': `${ResourcePath}/unused`, '--configuration-path': `${ResourcePath}/unused/configuration.json` });
   test.is(await process.whenExit(), 0);
 });
 
-Test.serial('check-dependency --project-path missing --report-missing', async test => {
+Test('check-dependency --project-path missing --report-missing', async test => {
   let process = new CheckDependencyProcess({ '--project-path': `${ResourcePath}/missing`, '--report-missing': true });
   test.is(await process.whenExit(), 1);
 });
 
-Test.serial('check-dependency --project-path missing --no-report-missing', async test => {
+Test('check-dependency --project-path missing --no-report-missing', async test => {
   let process = new CheckDependencyProcess({ '--project-path': `${ResourcePath}/missing`, '--no-report-missing': true });
   test.is(await process.whenExit(), 0);
 });
 
-Test.serial('check-dependency --project-path unused --report-unused', async test => {
+Test('check-dependency --project-path unused --report-unused', async test => {
   let process = new CheckDependencyProcess({ '--project-path': `${ResourcePath}/unused`, '--report-unused': true });
   test.is(await process.whenExit(), 1);
 });
 
-Test.serial('check-dependency --project-path unused --no-report-unused', async test => {
+Test('check-dependency --project-path unused --no-report-unused', async test => {
   let process = new CheckDependencyProcess({ '--project-path': `${ResourcePath}/unused`, '--no-report-unused': true });
   test.is(await process.whenExit(), 0);
 });
 
-Test.serial('check-dependency --project-path no-issue --report-used', async test => {
+Test('check-dependency --project-path no-issue --report-used', async test => {
   let process = new CheckDependencyProcess({ '--project-path': `${ResourcePath}/no-issue`, '--report-used': true });
   test.is(await process.whenExit(), 0);
 });
 
-Test.serial('check-dependency --project-path error', async test => {
+Test('check-dependency --project-path error', async test => {
   let process = new CheckDependencyProcess({ '--project-path': `${ResourcePath}/error` });
   test.is(await process.whenExit(), 2);
 });
