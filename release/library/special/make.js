d7821225bfa1db36e24fac578665ba81c0c69710
virtualpatterns.com <code@virtualpatterns.com>
2021-05-18 00:13:35 -0400

    post-test

--- release/library/special/make.js
+++ release/library/special/make.js
@@ -32,27 +32,29 @@ export async function Make(filePath, packageDependency, packagePath) {
 
     let dependencyBinary = await GetDependencyBinary(packageDependency, (await FileSystem.pathExists(`${packagePath}/node_modules`)) ? `${packagePath}/node_modules` : packagePath);
 
-    /*
-      Assumming shx is the binary, supports ...
-      shx
-      @shx
-      -shx
-      @-shx
-      -@shx
-      shx X
-      @-X shx
-      -@X shx X
-    */
-
     let { ast: fileAst } = Parse(await FileSystem.readFile(filePath, { 'encoding': 'utf-8' }));
     let fileRecipe = Query.query(fileAst, '$..recipe[*]');
 
     fileDependency = dependencyBinary.
     filter((binary) => {
+
+      /*
+        Assumming shx is the binary, supports ...
+        shx
+        @shx
+        -shx
+        @-shx
+        -@shx
+        shx X
+        X shx
+        X shx X
+      */
+
       let binaryPattern = new RegExp(`^[@|-]{0,2}${binary.binaryName}$|^[@|-]{0,2}${binary.binaryName}\\s+.*$|.*\\s+${binary.binaryName}\\s+.*|.*\\s+${binary.binaryName}$`, 'm');
       return fileRecipe.
       filter((recipe) => binaryPattern.test(recipe)).
       length > 0;
+
     }).
     map((binary) => binary.packageName).
     filter((dependency, index, fileDependency) => fileDependency.indexOf(dependency) === index);
