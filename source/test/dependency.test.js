863e2fd2a98d356dbf6d13db22fa68bc2a0d7ff8
virtualpatterns.com <code@virtualpatterns.com>
2021-08-25 23:57:43 -0400

    updates

--- source/test/dependency.test.js
+++ source/test/dependency.test.js
@@ -2,13 +2,9 @@ import Test from 'ava'
 
 import { Check } from '../index.js'
 
-const Process = process
-
 Test('default', async (test) => {
 
-  let dependency = await Check(Process.cwd())
-
-  // test.log(dependency.used)
+  let dependency = await Check()
   
   test.deepEqual(dependency.missing, {})
   test.deepEqual(dependency.unused, [])
