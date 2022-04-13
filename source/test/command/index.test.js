ba41af6a7ab3e3717d73d010d2f15b80306b951b
virtualpatterns.com <code@virtualpatterns.com>
2022-04-12 20:56:10 -0400

    updates

--- source/test/command/index.test.js
+++ source/test/command/index.test.js
@@ -1,5 +1,5 @@
-import { CreateLoggedProcess } from '@virtualpatterns/mablung-worker/test'
-import { ForkedProcess } from '@virtualpatterns/mablung-worker'
+import { CreateRandomId } from '@virtualpatterns/mablung-worker'
+import { LoggedForkedProcess } from '@virtualpatterns/mablung-worker/test'
 import FileSystem from 'fs-extra'
 import Path from 'path'
 import Test from 'ava'
@@ -8,76 +8,84 @@ import URL from 'url'
 const FilePath = URL.fileURLToPath(import.meta.url)
 const FolderPath = Path.dirname(FilePath)
 
-const LogPath = FilePath.replace('/release/', '/data/').replace(/\.test\.c?js$/, '.log')
-const LoggedProcess = CreateLoggedProcess(ForkedProcess, LogPath)
+const DataPath = FilePath.replace('/release/', '/data/').replace('.test.js', '')
 const ResourcePath = Path.resolve(FolderPath, 'resource')
 
 Test.before(async () => {
-  await FileSystem.ensureDir(Path.dirname(LogPath))
-  return FileSystem.remove(LogPath)
+  await FileSystem.remove(DataPath)
+  return FileSystem.ensureDir(DataPath)
+})
+
+Test.beforeEach(async (test) => {
+
+  let id = await CreateRandomId()
+  let logPath = Path.resolve(DataPath, `${id}.log`)
+
+  test.context.logPath = logPath
+
 })
 
 Test('default', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'))
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'))
   test.is(await process.whenExit(), 0)
 })
 
 Test('--help', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { '--help': true })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { '--help': true })
   test.is(await process.whenExit(), 0)
 })
 
 Test('--project-path section', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'section') })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'section') })
   test.is(await process.whenExit(), 1)
 })
 
 Test('--project-path section --no-report-section', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'section'), '--no-report-section': true })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'section'), '--no-report-section': true })
   test.is(await process.whenExit(), 0)
 })
 
 Test('--project-path no-issue', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'no-issue') })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'no-issue') })
   test.is(await process.whenExit(), 0)
 })
 
 Test('--project-path unused', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'unused') })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'unused') })
   test.is(await process.whenExit(), 1)
 })
 
 Test('--project-path unused --configuration-path unused/configuration.json', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'unused'), '--configuration-path': Path.resolve(ResourcePath, 'unused/configuration.json') })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'unused'), '--configuration-path': Path.resolve(ResourcePath, 'unused/configuration.json') })
   test.is(await process.whenExit(), 0)
 })
 
 Test('--project-path missing --report-missing', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'missing'), '--report-missing': true })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'missing'), '--report-missing': true })
   test.is(await process.whenExit(), 1)
 })
 
 Test('--project-path missing --no-report-section --no-report-missing', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'missing'), '--no-report-section': true, '--no-report-missing': true })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'missing'), '--no-report-section': true, '--no-report-missing': true })
   test.is(await process.whenExit(), 0)
 })
 
 Test('--project-path unused --report-unused', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'unused'), '--report-unused': true })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'unused'), '--report-unused': true })
   test.is(await process.whenExit(), 1)
 })
 
 Test('--project-path unused --no-report-unused', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'unused'), '--no-report-unused': true })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'unused'), '--no-report-unused': true })
   test.is(await process.whenExit(), 0)
 })
 
 Test('--project-path no-issue --report-used', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'no-issue'), '--report-used': true })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'no-issue'), '--report-used': true })
   test.is(await process.whenExit(), 0)
 })
 
 Test('--project-path error', async (test) => {
-  let process = new LoggedProcess(Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'error') })
+  let process = new LoggedForkedProcess(test.context.logPath, Path.resolve(FolderPath, '../../command/index.js'), { '--project-path': Path.resolve(ResourcePath, 'error') })
   test.is(await process.whenExit(), 2)
 })
